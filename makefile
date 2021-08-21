e649d4bf3885f013246037404069e59f9bed1df6
Scott Dauer <swdauer@gmail.com>
2021-08-21 17:14:59 -0500

    finish first attempt at quickestimate

--- makefile
+++ makefile
@@ -5,4 +5,5 @@ create:
 	cp --no-clobber c-template.c $(PROBLEM)/main.c
 	cp --no-clobber makefile-template $(PROBLEM)/makefile
 	cp --no-clobber readme-template.md $(PROBLEM)/README.md
+	touch $(PROBLEM)/input1.txt
 	
--- quickestimate/main.c
+++ quickestimate/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+  int n, i;
+  char num[200];
+  scanf(" %d", &n);
+  for (i = 0; i < n; i++)
+  {
+    scanf(" %s", num);
+    printf("%lu\n", strlen(num));
+  }
   return 0;
 }
