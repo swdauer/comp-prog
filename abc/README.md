e6a10d32eceb7e571cd6583d33e1d208fd9d0a5f
Scott Dauer <swdauer@gmail.com>
2021-08-22 14:34:31 -0500

    first attempt at abc

--- abc/README.md
+++ abc/README.md
@@ -1,5 +1,9 @@
-# PROBLEM NAME
+# abc
 
-\<problemlink\>
+<https://open.kattis.com/problems/abc>
 
 ## My Thoughts
+
+Here I'll just do a simple comparison between the 3 input numbers to sort them then I'll iterate over the second line's string and print the numbers as I do. The question is: how do I minimize the number of comparisons between the 3 input numbers to sort them?
+
+I think it's two comparisons in best case and three in worst case.
--- abc/main.c
+++ abc/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+
+void swap(int *x, int *y)
+{
+  int hold = *x;
+  *x = *y;
+  *y = hold;
+}
+
 int main()
 {
+  int a, b, c, i;
+  char order[4];
+  scanf(" %d %d %d", &a, &b, &c);
+  if (MAX(a, b) == a)
+  {
+    swap(&a, &b);
+  }
+  if (MAX(b, c) == b)
+  {
+    swap(&b, &c);
+    if (MAX(a, b) == a)
+    {
+      swap(&a, &b);
+    }
+  }
+
+  scanf(" %3s", order);
+
+  for (i = 0; i < 3; i++)
+  {
+    switch (order[i])
+    {
+    case 'A':
+      printf("%d", a);
+      break;
+    case 'B':
+      printf("%d", b);
+      break;
+    default:
+      printf("%d", c);
+    }
+    if (i != 2)
+      printf(" ");
+  }
+  printf("\n");
   return 0;
 }
