57deb14f391a12b21db5ac6411928b2aa8f10f4e
Scott Dauer <swdauer@gmail.com>
2021-08-23 15:32:24 -0500

    first attempt at main

--- peasoup/README.md
+++ peasoup/README.md
@@ -3,4 +3,5 @@
 <https://open.kattis.com/problems/peasoup>
 
 ## My Thoughts
-I'll remember the restaurant name and read each menu item. While I'm reading the items I'll check if it is either "pancakes" or "pea soup". I'll just use booleans to store whether or not I've found both.
+
+I'll remember the restaurant name and read each menu item. While I'm reading the items I'll check if it is either "pancakes" or "pea soup". I'll just use single bit flags to store whether or not I've found both.
--- peasoup/main.c
+++ peasoup/main.c
@@ -1,14 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
 #define PEA_SOUP "pea soup"
 #define PANCAKES "pancakes"
+#define PS_PAN_LENS 8
 #define UNSATISFIED "Anywhere is fine I guess\n"
-#define HAS_PEA_SOUP(X) ((X) | 1)        // turn on least significant bit as flag
-#define HAS_PANCAKES(X) ((X) | (1 << 1)) // turn on second to least significant bit as flag
+#define FLAG_PEA_SOUP 1      // turn on least significant bit as flag
+#define FLAG_PANCAKES 1 << 1 // turn on second to least significant bit as flag
 
 int main()
 {
-  char test = 0;
-  printf("%u %u\n", HAS_PANCAKES(test), HAS_PEA_SOUP(test));
+  int n, i;
+  scanf(" %d", &n);
+  for (i = 0; i < n; i++)
+  {
+    int k, j;
+    char restaurant[101], flags = 0;
+    scanf(" %d", &k);
+    fgets(restaurant, 101, stdin);
+    for (j = 0; j < k; j++)
+    {
+      char item[101];
+      int itemLen;
+      fgets(item, 101, stdin);
+      itemLen = strlen(item);
+      if (item[itemLen - 1] == '\n')
+      {
+        item[itemLen - 1] = '\0';
+        itemLen--;
+      }
+
+      if (itemLen == PS_PAN_LENS)
+      {
+        if (strcmp(item, PEA_SOUP) == 0)
+        {
+          flags = flags | FLAG_PEA_SOUP;
+        }
+        else if (strcmp(item, PANCAKES) == 0)
+        {
+          flags = flags | FLAG_PANCAKES;
+        }
+      }
+
+      if (flags == (flags | FLAG_PANCAKES | FLAG_PEA_SOUP))
+      {
+        printf("%s\n", restaurant);
+        return 0;
+      }
+    }
+  }
+  printf(UNSATISFIED);
   return 0;
 }
