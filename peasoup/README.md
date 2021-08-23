a01dec128fd98512f1e2b3e3b26f241fc69ca51e
Scott Dauer <swdauer@gmail.com>
2021-08-23 15:12:48 -0500

    add macros

--- peasoup/README.md
+++ peasoup/README.md
@@ -1,5 +1,6 @@
-# PROBLEM NAME
+# peasoup
 
-\<problemlink\>
+<https://open.kattis.com/problems/peasoup>
 
 ## My Thoughts
+I'll remember the restaurant name and read each menu item. While I'm reading the items I'll check if it is either "pancakes" or "pea soup". I'll just use booleans to store whether or not I've found both.
--- peasoup/main.c
+++ peasoup/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#define PEA_SOUP "pea soup"
+#define PANCAKES "pancakes"
+#define UNSATISFIED "Anywhere is fine I guess\n"
+#define HAS_PEA_SOUP(X) ((X) | 1)        // turn on least significant bit as flag
+#define HAS_PANCAKES(X) ((X) | (1 << 1)) // turn on second to least significant bit as flag
+
 int main()
 {
+  char test = 0;
+  printf("%u %u\n", HAS_PANCAKES(test), HAS_PEA_SOUP(test));
   return 0;
 }
