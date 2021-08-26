6c2f68324f77503d1d44005bb8623b4e57a5e7ec
Scott Dauer <swdauer@gmail.com>
2021-08-26 10:25:51 -0500

    add check that correct value is within lower and upper bounds

--- guessinggame/main.hs
+++ guessinggame/main.hs
@@ -5,18 +5,21 @@ readInput :: String -> [String]
 readInput s = lines s
 
 solve :: [String] -> [String]
-solve xs = playGames 1 10 xs
+solve xs = playGames 0 11 xs
 
 playGames :: Int -> Int -> [String] -> [String]
-playGames _ _ (x:[]) = [] 
-playGames lower upper (x:"right on":xs) | lower > upper = "Stan is dishonest" : playGames 1 10 xs
-                                        | otherwise = "Stan may be honest" : playGames 1 10 xs
+playGames 0 11 ("0":_) = [] 
+playGames lower upper ("0":_) | lower >= upper - 1 = "Stan is dishonest" : []
+                               | otherwise = "Stan may be honest" : []
+playGames lower upper (x:"right on":xs) | (lower < upper - 1) && (xVal > lower) && (xVal < upper) = "Stan may be honest" : playGames 0 11 xs
+                                        | otherwise = "Stan is dishonest" : playGames 0 11 xs
+                                        where xVal = read x
 playGames lower upper (x:"too high":xs) | xVal < upper = playGames lower xVal xs
                                         | otherwise = playGames lower upper xs
-                                        where xVal = read x - 1
+                                        where xVal = read x
 playGames lower upper (x:"too low":xs) | xVal > lower = playGames xVal upper xs
                                        | otherwise = playGames lower upper xs
-                                       where xVal = read x + 1
+                                       where xVal = read x
 
 writeOutput :: [String] -> String
 writeOutput xs = unlines xs
