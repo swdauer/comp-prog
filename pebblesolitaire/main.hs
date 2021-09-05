aae21ddb58c13a3f42cadfd6b1887226a4ae85dc
Scott Dauer <swdauer@gmail.com>
2021-09-05 16:03:56 -0500

    add addMin function and make first submission attempt

--- pebblesolitaire/main.hs
+++ pebblesolitaire/main.hs
@@ -10,16 +10,23 @@ main :: IO ()
 main = interact(writeOutput . solve . readInput)
 
 solve :: [String] -> [Int]
-solve = map (pebbleCount)
+solve = map (findMin 1000) . map (\s -> s:[])
 
 pebbleCount :: String -> Int
 pebbleCount = foldl (\x c -> if c == 'o' then x + 1 else x) 0
 
-minOr1 :: [Int] -> Int -> Int
-minOr1 (1:xs) _ = 1
-minOr1 [] currMin = currMin
-minOr1 (x:xs) currMin | x < currMin = minOr1 xs x
-                      | otherwise = minOr1 xs currMin
+-- minOr1 :: [Int] -> Int -> Int
+-- minOr1 (1:xs) _ = 1
+-- minOr1 [] currMin = currMin
+-- minOr1 (x:xs) currMin | x < currMin = minOr1 xs x
+--                       | otherwise = minOr1 xs currMin
+
+findMin :: Int -> [String] -> Int
+findMin min [] = min
+findMin min (x:xs) | curr == 1 = 1
+                   | curr < min = findMin curr ((reverse . tail . reverse . descendants) x ++ xs)
+                   | otherwise = findMin min ((reverse . tail . reverse . descendants) x ++ xs)
+                   where curr = pebbleCount x
 
 descendants :: String -> [String]
 descendants (x:y:[]) = (x:y:""):[]
