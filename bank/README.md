10562b8782bcce4b7b8bc9cc47cbb0e1d1b0d337
Scott Dauer <swdauer@gmail.com>
2021-12-06 20:55:18 -0600

    Fist attempt at findBest but does not pass first test

--- bank/README.md
+++ bank/README.md
@@ -3,3 +3,5 @@
 <https://open.kattis.com/problems/bank>
 
 ## My Thoughts
+
+This is a recursive problem. At each step, the included element will be one of the largest numbers at one of the times. I don't think the second largest number at a time will ever be included on a step.
---
+++ bank/main.hs
@@ -0,0 +1,39 @@
+main :: IO ()
+main = interact(writeOutput . solve . readInput)
+
+readInput :: String -> [(Integer, Integer)]
+readInput = map (\x -> (twoElemListToPair . words) x) . lines
+
+twoElemListToPair :: [String] -> (Integer, Integer)
+twoElemListToPair [x,y] = (read x, read y)
+
+solve :: [(Integer, Integer)] -> Integer
+solve (x:xs) = let zs = sort xs
+               in findBest 0 (snd x) 0 zs
+
+sort :: [(Integer, Integer)] -> [(Integer, Integer)]
+sort [] = []
+sort (x:xs) = sort [y | y <- xs, snd y < snd x || (snd y == snd x && fst y > fst x)]
+            ++ [x]
+            ++ sort [y | y <- xs, snd y > snd x || (snd y == snd x && fst y <= fst x)]
+
+writeOutput :: Integer -> String
+writeOutput x = shows x "\n"
+
+-- parameters:
+--  current time
+--  total time
+--  total money accumulator
+--  list of monies
+--  total money
+findBest :: Integer -> Integer -> Integer -> [(Integer, Integer)] -> Integer
+findBest _ _ money [] = money
+findBest curr time money (x:xs) | curr == time = money
+                                | snd x < curr = findBest curr time money xs
+                                | otherwise    = max (findBest (curr+1) time (money+fst x) xs) 
+                                                     (findBest curr time money (shave (snd x) xs))
+
+shave :: Integer -> [(Integer, Integer)] -> [(Integer, Integer)]
+shave _ [] = []
+shave i (x:xs) | snd x == i = shave i xs
+               | otherwise  = x:xs
