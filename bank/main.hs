4a54d0695587a5c460d2c210599d9314240ca24a
Scott Dauer <swdauer@gmail.com>
2021-12-07 19:59:10 -0600

    First attempt exceeded time limit Add pareDown function to speed execution

--- bank/main.hs
+++ bank/main.hs
@@ -9,13 +9,19 @@ twoElemListToPair [x,y] = (read x, read y)
 
 solve :: [(Integer, Integer)] -> String
 solve (x:xs) = let zs = sort xs
-               in show (findBest 0 (snd x) 0 zs)
+               in show (findBest 0 (snd x) 0 (pareDown 0 (-1) zs))
 
 sort :: [(Integer, Integer)] -> [(Integer, Integer)]
 sort [] = []
 sort (x:xs) = sort [y | y <- xs, snd y < snd x || (snd y == snd x && fst y > fst x)]
             ++ [x]
             ++ sort [y | y <- xs, snd y > snd x || (snd y == snd x && fst y <= fst x)]
+
+pareDown :: Integer -> Integer -> [(Integer, Integer)] -> [(Integer, Integer)]
+pareDown _ _ [] = []
+pareDown count elem (x:xs) | snd x == elem && count == elem + 1 = pareDown count elem xs
+                           | snd x /= elem = x : pareDown 1 (snd x) xs
+                           | otherwise = x : pareDown (count+1) elem xs
 
 writeOutput :: String -> String
 writeOutput x = x ++ "\n"
