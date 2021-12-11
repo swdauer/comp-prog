11c915c6c4867f309244e57ad270d38bc20fb863
Scott Dauer <swdauer@gmail.com>
2021-12-10 20:43:09 -0600

    Bank solution accepted

--- bank/README.md
+++ bank/README.md
@@ -4,4 +4,8 @@
 
 ## My Thoughts
 
-This is a recursive problem. At each step, the included element will be one of the largest numbers at one of the times. I don't think the second largest number at a time will ever be included on a step.
+If I proceed backwards from the end of the day, I think I can take the greedy approach of just taking the max amount of money at each step.
+
+I'll maintain a current time (starts at T-1) and a list of customers sorted by amount of money decreasing.
+
+I'll iterate and at each iteration I'll take the first element of the list whose time is >= to the current time. Once the current time is -1 I return the total.
--- bank/main.hs
+++ bank/main.hs
@@ -2,44 +2,31 @@ main :: IO ()
 main = interact(writeOutput . solve . readInput)
 
 readInput :: String -> [(Integer, Integer)]
-readInput = map (\x -> (twoElemListToPair . words) x) . lines
+readInput = map (twoElemListToPair . words) . lines
 
 twoElemListToPair :: [String] -> (Integer, Integer)
 twoElemListToPair [x,y] = (read x, read y)
 
-solve :: [(Integer, Integer)] -> String
+solve :: [(Integer, Integer)] -> String -> String
 solve (x:xs) = let zs = sort xs
-               in show (findBest 0 (snd x) 0 (pareDown 0 (-1) zs))
+               in shows (findTotal 0 (snd x - 1) [] zs)
 
 sort :: [(Integer, Integer)] -> [(Integer, Integer)]
 sort [] = []
-sort (x:xs) = sort [y | y <- xs, snd y < snd x || (snd y == snd x && fst y > fst x)]
+sort (x:xs) = sort [y | y <- xs, fst y > fst x || (fst y == fst x && snd y >= snd x)]
             ++ [x]
-            ++ sort [y | y <- xs, snd y > snd x || (snd y == snd x && fst y <= fst x)]
+            ++ sort [y | y <- xs, fst y < fst x || (fst y == fst x && snd y < snd x)]
 
-pareDown :: Integer -> Integer -> [(Integer, Integer)] -> [(Integer, Integer)]
-pareDown _ _ [] = []
-pareDown count elem (x:xs) | snd x == elem && count == elem + 1 = pareDown count elem xs
-                           | snd x /= elem = x : pareDown 1 (snd x) xs
-                           | otherwise = x : pareDown (count+1) elem xs
+-- params:
+--  the total amount of money (accumulator)
+--  the current time
+--  the list to be appended back
+--  the sorted list
+findTotal :: Integer -> Integer -> [(Integer, Integer)] -> [(Integer, Integer)] -> Integer
+findTotal total (-1) _ _=            total
+findTotal total currTime toAppend [] = findTotal total (currTime-1) [] (reverse toAppend) -- couldn't find a customer with a high enough time
+findTotal total currTime toAppend (x:xs) | snd x >= currTime = findTotal (total+fst x) (currTime-1) [] (reverse toAppend++xs)
+                                         | otherwise = findTotal total currTime (x:toAppend) xs
 
-writeOutput :: String -> String
-writeOutput x = x ++ "\n"
-
--- parameters:
---  current time
---  total time
---  total money accumulator
---  list of monies
---  total money
-findBest :: Integer -> Integer -> Integer -> [(Integer, Integer)] -> Integer
-findBest _ _ money [] = money
-findBest curr time money (x:xs) | curr == time = money
-                                | snd x < curr = findBest curr time money xs
-                                | otherwise    = max (findBest (curr+1) time (money+fst x) xs) 
-                                                     (findBest curr time money (shave (snd x) xs))
-
-shave :: Integer -> [(Integer, Integer)] -> [(Integer, Integer)]
-shave _ [] = []
-shave i (x:xs) | snd x == i = shave i xs
-               | otherwise  = x:xs
+writeOutput :: (String -> String) -> String
+writeOutput x = x "\n"
