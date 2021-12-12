28da31b6e194d009be69090b657c1f1008719365
Scott Dauer <swdauer@gmail.com>
2021-12-12 10:35:14 -0600

    First attempt Wrong answer on 7th case

--- doggopher/README.md
+++ doggopher/README.md
@@ -3,3 +3,13 @@
 <https://open.kattis.com/problems/doggopher>
 
 ## My Thoughts
+
+Given the coordinates of the dog and the gopher, I need to determine if there is a hole which has euclidean distance from the dog greater than or equal to 2 times the euclidean distance from the gopher. If this is true than the gopher can escape through that hole.
+
+What this is equivalent to is if the square of the distance from the dog to hole is greater than or equal to 4 times the square of the distance from the gopher to the whole.
+
+Thus if the dog position is `(xd, yd)`, the gopher position is `(xg, yg)`, and the hole position is `(xh, yh)`; the gopher can escape if
+
+`4 * ((xg - xh)^2 + (yg - yh)^2) >= (xd - xh)^2 + (yd - yh)^2`
+
+I'll also read the numbers in as integers instead of floating point. Otherwise the equal case might not calculate properly. I can account for this by effectively multiplying each coordinate by 1000.
--- doggopher/main.hs
+++ doggopher/main.hs
@@ -1,1 +1,34 @@
 module Main where
+
+main :: IO ()
+main = interact(writeOutput . solve . readInput)
+
+readInput :: String -> [String]
+readInput = lines
+
+solve :: [String] -> ShowS
+solve input = let (gopher:dog:holes) = parseInput True input
+              in determineIfEscape gopher dog holes
+
+parseInput :: Bool -> [String] -> [(Integer, Integer)]
+parseInput _ [] = []
+parseInput True (line:input) = let [xg, yg, xd, yd] = map (decimalToInteger id) (words line)
+                               in (xg, yg):(xd, yd):parseInput False input
+parseInput False (line:input) = let [x, y] = map (decimalToInteger id) (words line)
+                                in (x, y):parseInput False input
+
+decimalToInteger :: (String -> String) -> String -> Integer
+decimalToInteger beginning ('.':rest) = 1000 * (read . beginning) "" + read rest
+decimalToInteger beginning (x:rest)   = decimalToInteger (beginning . (x :)) rest
+
+determineIfEscape :: (Integer, Integer) -> (Integer, Integer) -> [(Integer, Integer)] -> ShowS
+determineIfEscape _ _ [] = ("The gopher cannot escape." ++)
+determineIfEscape (xg, yg) (xd, yd) ((xh, yh):points) | 4 * ((xg - xh)^2 + (yg - yh)^2)
+                                                        <= (xd - xh)^2 + (yd - yh)^2
+                                                        = ("The gopher can escape through the hole at (" ++)
+                                                          . shows (div xh 1000) . ('.':) . shows (mod xh 1000) . (',':)
+                                                          . shows (div yh 1000) . ('.':) . shows (mod yh 1000) . (")." ++)
+                                                      | otherwise = determineIfEscape (xg, yg) (xd, yd) points
+
+writeOutput :: ShowS -> String
+writeOutput s = s "\n"
