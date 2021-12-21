6ae50c15fd9a9675103218c168e7863ed3b92744
Scott Dauer <swdauer@gmail.com>
2021-12-20 21:20:18 -0600

    Attempt faster sieve method

--- industrialspy/README.md
+++ industrialspy/README.md
@@ -13,3 +13,9 @@
 3. Print all the cases' answers
 
 Looks like this approach is too long, unless there is a way to speed up my sieving process
+
+There is by using an iterating integer to remove elements instead of checking mod each time.
+
+Even with the improved sieve method it's still too slow.
+
+So now I'll do a method of permuting the string, checking if each permutation is prime, and memoizing the results.
--- industrialspy/main.hs
+++ industrialspy/main.hs
@@ -14,7 +14,13 @@ constructSieve n = sieve [2..n]
 
 sieve :: [Int] -> [Int]
 sieve []     = []
-sieve (x:xs) = x : sieve (filter (\y -> y `mod` x /= 0) xs)
+sieve (x:xs) = (:) x $ sieve $ removeMultN x x xs
+
+removeMultN :: Int -> Int -> [Int] -> [Int]
+removeMultN _ _ [] = []
+removeMultN n curr (x:xs) | x == curr = removeMultN n (curr + n) xs
+                          | x > curr = removeMultN n (curr + n) (x:xs)
+                          | otherwise = x : removeMultN n curr xs
 
 writeOutput :: ShowS -> String
 writeOutput s = s "\n"
