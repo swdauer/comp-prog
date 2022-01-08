68e685fad869c2c7eee78d87a0bd050877cce7d9
Scott Dauer <swdauer@gmail.com>
2022-01-07 23:14:29 -0600

    Add table constructors

--- playfair/README.md
+++ playfair/README.md
@@ -5,3 +5,10 @@
 ## My Thoughts
 
 There isn't a lot of input here so I'm not worried about speed. 
+
+The operations I need on the table are too
+
+- Find an element and return its row and column
+- Access an element given its row and column
+
+Since the table is only 5 by 5 I'm not worried about having quick access to a row and column. Thus I'll structure the table as a list of lists.
--- playfair/main.hs
+++ playfair/main.hs
@@ -1,3 +1,22 @@
 module Main where
 
+import           Data.Char (isLetter)
+
 main = print 3
+
+-- constructTable table [] = table
+-- constructTable table (s:ss) | not (isLetter s) || s `inTable` table = constructTable table ss
+--                             | otherwise = constructTable (addToTable s table) ss
+
+constructTable :: [Char] -> [[Char]]
+constructTable = foldl (\table c -> if not (isLetter c) || c `inTable` table
+                                    then table
+                                    else addToTable c table) [[]]
+
+constructFullTable str = constructTable (str ++ ['a'..'p'] ++ ['r'..'z'])
+
+inTable s = foldl (\b r -> b || (s `elem` r)) False
+
+addToTable c [[]] = [[c]]
+addToTable c (r:table) | length r == 5 = [c]:r:table
+                       | otherwise = (c:r) : table
